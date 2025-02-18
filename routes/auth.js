cc1fd0d3726fe8333c76dda5c2e502424f49cbd3
Travis Souther <souther-trav@hotmail.com>
2025-02-18 16:48:15 -0600

    Added messagely(help needed

---
+++ routes/auth.js
@@ -0,0 +1,54 @@
+const jwt = require("jsonwebtoken");
+const Router = require("express").Router;
+const router = new Router();
+
+const User = require("../models/user");
+const {SECRET_KEY} = require("../config");
+const ExpressError = require("../expressError");
+
+
+/** login: {username, password} => {token} */
+
+router.post("/login", async function (req, res, next) {
+  try {
+    console.log(req.body); // This will show the contents of the request body
+
+    let { username, password } = req.body;
+
+    if (!username || !password) {
+      throw new ExpressError("Username or password missing", 400);
+    }
+
+    if (await User.authenticate(username, password)) {
+      let token = jwt.sign({ username }, SECRET_KEY);
+      User.updateLoginTimestamp(username);
+      return res.json({ token });
+    } else {
+      throw new ExpressError("Invalid username/password", 400);
+    }
+  } catch (err) {
+    return next(err);
+  }
+});
+
+/** register user: registers, logs in, and returns token.
+ *
+ * {username, password, first_name, last_name, phone} => {token}.
+ */
+
+router.post("/register", async function (req, res, next) {
+  try {
+    let {username} = await User.register(req.body);
+    let token = jwt.sign({username}, SECRET_KEY);
+    User.updateLoginTimestamp(username);
+    return res.json({token});
+  }
+
+  catch (err) {
+    return next(err);
+  }
+});
+
+
+
+module.exports = router;
